f547b05038bad143eef510b9b3603ea19b0a3c78
Anthony Lee <don.anthony.lee@gmail.com>
2022-06-29 22:54:06 +0800

    update scripts

--- scripts/changelog-gen.sh
+++ scripts/changelog-gen.sh
@@ -4,12 +4,25 @@ if [ -z "$1" ]; then
 	exit 1
 fi
 
-echo ""
-echo "| 更改日期 | 更改信息 |"
-echo "| --- | --- |"
-#git log --no-merges --format="| %ad | #a1# %s #a2# %H #a3# |" --date=short $1 | sed -e '/IGNORE_CHANGELOG/d' -e '/TODO/d' -e 's/#a1# /[/g' -e 's/ #a2# /](https:\/\/github.com\/DonAnthonyLee\/DonAnthonyLee.github.io\/commit\//g' -e 's/ #a3#/)/g' -
+printf "Checking $1 ..."
+grep "{:.changelog}" $1 > /dev/null 2>&1
+if [ "x$?" != "x0" ]; then
+	printf "[NO NEED]\n"
+	exit 0
+fi
 
-git log --no-merges --format="| %ad | #a1# %H #a2# %s #a3# |" --date=short $1 | sed -e '/IGNORE_CHANGELOG/d' -e '/TODO/d' -e 's/#a1# /<a href=\"https:\/\/github.com\/DonAnthonyLee\/DonAnthonyLee.github.io\/commit\//g' -e 's/ #a2# /\" target=\"_blank\">/g' -e 's/ #a3#/<\/a>/g' -
+sed -i '/{:.changelog}/,$d' $1
+if [ "x$?" != "x0" ]; then
+	printf "\e[31m[FAILED]\e[0m\n"
+	exit 0
+fi
+
+echo "{:.changelog}" >> $1
+echo "" >> $1
+echo "| 更改日期 | 更改信息 |" >> $1
+echo "| --- | --- |" >> $1
+git log --no-merges --format="| %ad | #a1# %H #a2# %s #a3# |" --date=short $1 | sed -e '/IGNORE_CHANGELOG/d' -e '/TODO/d' -e 's/#a1# /<a href=\"https:\/\/github.com\/DonAnthonyLee\/DonAnthonyLee.github.io\/commit\//g' -e 's/ #a2# /\" target=\"_blank\">/g' -e 's/ #a3#/<\/a>/g' - >> $1
+printf "\e[32m[OK]\e[0m\n"
 
 exit 0
 
